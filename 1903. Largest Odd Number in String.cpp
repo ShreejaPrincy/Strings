c140642966d6c1635d0056017480eb1251405e11
Shreeja Princy <148578262+ShreejaPrincy@users.noreply.github.com>
2026-05-16 01:13:47 +0530

    Create 1903. Largest Odd Number in String.cpp

---
+++ 1903. Largest Odd Number in String.cpp
@@ -0,0 +1,15 @@
+// Time Complexity: O(n)
+// Space Complexity: O(1) extra space
+
+class Solution {
+public:
+    string largestOddNumber(string num) {
+
+        int i = num.size() - 1;
+
+        while(i >= 0 && (num[i] - '0') % 2 == 0)
+            i--;
+
+        return num.substr(0, i + 1);
+    }
+};
